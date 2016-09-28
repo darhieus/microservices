ded5ff5215fab1e923847ec7b4eb6b20de11d7d0
thefosk <marco@mashape.com>
2016-09-27 22:29:29 -0700

    wip

--- kong/plugins/header-router/handler.lua
+++ kong/plugins/header-router/handler.lua
@@ -1,4 +1,5 @@
 local BasePlugin = require "kong.plugins.base_plugin"
+local pl_stringx = require "pl.stringx"
 local url = require "socket.url"
 
 local HeaderRouterHandler = BasePlugin:extend()
@@ -11,7 +12,7 @@ function HeaderRouterHandler:access(conf)
   HeaderRouterHandler.super.access(self)
 
   local header_value = ngx.req.get_headers()[conf.header_name]
-  if header_value and header_value == conf.header_value then
+  if header_value and pl_stringx.strip(header_value) == conf.header_value then
     local parsed_url = url.parse(ngx.ctx.upstream_url)
     ngx.ctx.upstream_url = conf.upstream_url..parsed_url.path
   end
